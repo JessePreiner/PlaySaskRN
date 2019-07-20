9849a24b593412114061fd276e40ccd3d0ef0160
Jesse Preiner <jesse.preiner@pivotsubscriptions.com>
2019-07-20 11:38:24 -0600

    initial

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Maybe?</Text>
+    </View>
+  );
+};
+
+export default App;
